2198c20f75e0e98772b253d930e32f3a6af15d05
Daniel Pereira <daniel12fsp@yahoo.com.br>
2023-10-08 18:02:19 -0400

    feat: add serverless framework

--- lambda.ts
+++ lambda.ts
@@ -1,15 +1,15 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'src/app.module';
+import { setupNestApp } from 'src/main';
 
 let cachedServer;
 
 export const handler = async (event, context) => {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
+    const app = await setupNestApp();
+
+    await app.init();
     cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
+      app: app.getHttpAdapter().getInstance(),
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+export async function setupNestApp() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,6 +15,25 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Customers example')
+    .setDescription('The customers API description')
+    .setVersion('1.0')
+    .addServer(
+      'https://99fyxf8tea.execute-api.us-east-1.amazonaws.com/dev',
+      'production-server',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('documentation', app, document);
+
+  return app;
+}
+
+async function bootstrap() {
+  const app = await setupNestApp();
   await app.listen(3000);
 }
 bootstrap();
