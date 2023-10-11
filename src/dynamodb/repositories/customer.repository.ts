055d7685bcd84af651dd24644d253903aeed36f4
Daniel Pereira <daniel12fsp@yahoo.com.br>
2023-10-10 22:00:35 -0400

    feat: Add endpoint for search all fields

--- src/dynamodb/repositories/customer.repository.ts
+++ src/dynamodb/repositories/customer.repository.ts
@@ -6,4 +6,24 @@ import { ScanCommand } from '@aws-sdk/lib-dynamodb';
 @DynamoDBRepository()
 export class CustomerRepository extends BaseRepository<Customer> {
   protected tableName = 'customers';
+
+  async searchAllFields(searchTerm) {
+    const command = new ScanCommand({
+      TableName: this.tableName,
+      ExpressionAttributeValues: {
+        ':searchTerm': searchTerm,
+      },
+      FilterExpression:
+        'contains(#id, :searchTerm) OR contains(#name, :searchTerm) OR contains(#email, :searchTerm)',
+      ExpressionAttributeNames: {
+        '#id': 'id',
+        '#name': 'name',
+        '#email': 'email',
+      },
+      ConsistentRead: true,
+    });
+
+    const results = await this.docClient.send(command);
+    return results.Items;
+  }
 }
